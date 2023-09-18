7d588a50322c8ed8936360921abf411fd59e664f
Victor M <vmugicag@redhat.com>
2023-09-18 16:34:26 +0200

    fix

--- test/generate_coverage.sh
+++ test/generate_coverage.sh
@@ -3,8 +3,14 @@
 COVERAGE_DIRECTORY="$PWD/coverage"
 BATS_CMD='bats'
 TESTS_DIRECTORY='test'
+IGNORE_TAGS='!no-kcov'
+
+if [ -d "$COVERAGE_DIRECTORY" ]; then
+  rm -rf "$COVERAGE_DIRECTORY"
+fi
 
 kcov --include-pattern cicd-tools/src \
     "$COVERAGE_DIRECTORY" \
     "$BATS_CMD" \
+    --filter-tags "$IGNORE_TAGS" \
     "$TESTS_DIRECTORY"
